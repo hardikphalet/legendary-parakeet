86d62cdecca1f63e96f7d0f9c10fe780cb81d66e
hardikphalet <hardik.phalet@gmail.com>
2020-12-02 18:46:19 +0530

    Le premier fichier fonctionnait parfaitement.

--- basics/un.py
+++ basics/un.py
@@ -1,9 +1,11 @@
 import cv2 as cv
 import sys
-img = cv.imread(starry_night.jpg)
+img = cv.imread(
+    "D:/example.jpg")
 if img is None:
-    sys.exit("Could not read the image.")
+    sys.exit("Couldn't read the image")
 cv.imshow("Display window", img)
 k = cv.waitKey(0)
+
 if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+    cv.imwrite("example.png",img)
