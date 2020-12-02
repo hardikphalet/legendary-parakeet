348b41bb3198e1855ffcd15f6f01232f6f7bc8bc
hardikphalet <hardik.phalet@gmail.com>
2020-12-02 19:22:11 +0530

    Enregistrement d'un flux de caméra inversé.

---
+++ basics/quatre.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+fourcc = cv.VideoWriter_fourcc(*'DIVX')
+out = cv.VideoWriter('Output1.mp4', fourcc, 60.0, (640,480))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame")
+        break
+    frame = cv.flip(frame, 0)
+
+    out.write(frame)
+    cv.imshow('frame',frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+cap.release()
+out.release()
+cv.destroyAllWindows()
